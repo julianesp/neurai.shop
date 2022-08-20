589903bb6821f2d352045f69c8836d7791faeae9
Alex España <julii1295@gmail.com>
2022-08-20 12:31:32 -0500

    ADD menú burger

---
+++ src/js/menu.js
@@ -0,0 +1,12 @@
+/**
+ * Los elementos que va a llevar esta función son:
+ * - Botón que activa: panelBtn
+ * - El panel es el elemento que se va a mover: panel
+ */
+
+const d = document
+
+//mediante la delegación de eventos
+d.addEventListener
+
+// Todo lo que tenga que ver con el DOM se maneja con "document"
