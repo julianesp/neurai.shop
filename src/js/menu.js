797a614404b39d82bab3e7af2fadb08c43c83ce3
Alex España <julii1295@gmail.com>
2022-08-20 16:30:51 -0500

    Botón funcionando

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,9 +4,21 @@
  * - El panel es el elemento que se va a mover: panel
  */
 
-const d = document
+export default function hamburgerMenu(panelBtn, panel) {
+    const d = document
+    
+    // se agregará un evento
+    d.addEventListener('click', e => {
+        /**
+         * si el objeto que origina el evento, coincide con el selector
+         * que me das en la variable panelBtn
+         *  */ 
+        if (e.target.matches(panelBtn)) {
+            // here se intercambia esa clase 'is-active' para quitarla o ponerla
+            d.querySelector(panel).classList.toggle('is-active')
+        }
+    })    
+}
 
-//mediante la delegación de eventos
-d.addEventListener
 
 // Todo lo que tenga que ver con el DOM se maneja con "document"
