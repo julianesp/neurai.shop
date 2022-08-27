5fbf7e4f517c254879b635b3d498491f7267f8df
Alex España <julii1295@gmail.com>
2022-08-27 11:25:50 -0500

    Modificando diseño de publicidad

--- src/containers/Publicidad.jsx
+++ src/containers/Publicidad.jsx
@@ -5,11 +5,21 @@ const Publicidad = () => {
     return (
         <section className="negocios">
             <ul className='negocios-contenedor'>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>
+                    <button>anuncio1 atacar a cada botton</button>
+                </li>
+                <li>
+                    <button>anuncio2</button>
+                </li>
+                <li>
+                    <button>anuncio3</button>
+                </li>
+                <li>
+                    <button>anuncio4</button>
+                </li>
+                <li>
+                    <button>anuncio5</button>
+                </li> 
             </ul>
         </section>
     );
