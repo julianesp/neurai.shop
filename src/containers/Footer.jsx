52ab59361d7fe3ba9d0d85cc4cdd29827ab099d8
Alex España <julii1295@gmail.com>
2022-08-27 23:24:00 -0500

    Publicidad en ancho completo || Imagen de fondo en el footer

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -3,20 +3,23 @@ import '@styles/Footer.scss'
 import dev from '@img/developer_2.jpg'
 import sena1 from "@img/logos/sena.png"
 import platzi from "@img/logos/platzi.jpg"
+import park from '@img/presentacion/park.jpg'
 
 const Footer = () => {
     return (
         <div className="footer">
+            <img src={park} alt="Parque Colón Putumayo" />
+
             <section className="info-dev">
-                <h4 className="gradiente-text">Desarrollado por:<br/>Julián España</h4>
                 <img src={dev} alt="Desarrollador"/>
                 <div className="education">
+                    <h4>Desarrollado por:<br/>Julián España</h4>
                     <h5>Técnico en sistemas </h5>
-                    <img src={sena1} alt="Logo SENA"/>
+                    {/* <img src={sena1} alt="Logo SENA"/> */}
                     <h5>Tecnólogo en Análisis de sistemas</h5>
-                    <img src={sena1} alt="Logo SENA"/>
+                    {/* <img src={sena1} alt="Logo SENA"/> */}
                     <h5>Desarrollador Front-End</h5>
-                    <img src={platzi} alt="Logo Platzi"/>
+                    {/* <img src={platzi} alt="Logo Platzi"/> */}
                 </div>
             </section>
         </div>
