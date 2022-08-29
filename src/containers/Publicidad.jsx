40ffded4cc2ca0923d51179a32672c0b2a7ee9d8
Alex España <julii1295@gmail.com>
2022-08-29 07:45:02 -0500

    Reduciendo ancho de imagen de publicidad

--- src/containers/Publicidad.jsx
+++ src/containers/Publicidad.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import '@styles/Publicidad.scss'
 
 const imgs = require.context('@img/', true)
+const parque = require.context('@img/presentacion/', true)
 
 const Publicidad = () => {
     return (
         <section className="negocios">
             <ul className='negocios-contenedor'>
                 <li>                    
-                    <img src={imgs(`./developer.jpg`)} alt="" />
+                    <img src={parque(`./park.jpg`)} alt="" />
                 </li>
                 <li>
                     <img src={imgs(`./developer.jpg`)} alt="" />
