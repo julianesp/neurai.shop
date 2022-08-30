5dc62b37e8cb29702b5fba00657a4ba13a74173c
Alex España <julii1295@gmail.com>
2022-08-30 11:00:44 -0500

    Fijando ancho de 320px para la publicidad

--- src/components/Anuncios.jsx
+++ src/components/Anuncios.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import '@styles/Anuncios.scss';
+// import '@img/'
 
 const Anuncios = () => {
     return (
         <section className='container'>
-            <div className="contenido">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. At culpa labore assumenda, eius sit nemo ad earum voluptatibus cumque quam eaque fugit accusamus mollitia possimus non asperiores sapiente placeat. Laboriosam?
-                </p>
+            <div className="title">
+                <h3>Tiendas del pueblo</h3>
             </div>
-            <div className="contenido">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. At culpa labore assumenda, eius sit nemo ad earum voluptatibus cumque quam eaque fugit accusamus mollitia possimus non asperiores sapiente placeat. Laboriosam?
-                </p>
-            </div>
-            <div className="contenido">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. At culpa labore assumenda, eius sit nemo ad earum voluptatibus cumque quam eaque fugit accusamus mollitia possimus non asperiores sapiente placeat. Laboriosam?
-                </p>
+
+            <div className="stores">
+                <div className="contenido">
+                    <img src='../assets/img/shop.png' alt="" />
+                </div>
+                <div className="contenido">
+                    <img src='../assets/img/shop.png' alt="" />
+                </div>
+                <div className="contenido">
+                    <img src='../assets/img/shop.png' alt="" />
+                </div>
             </div>
         </section>
     );
