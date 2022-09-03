45dd6c346b0f9053df7196eb5f7b63f75ce4f1ac
Alex España <julii1295@gmail.com>
2022-09-02 21:37:45 -0500

    Anuncios bien posicionados

--- src/containers/Anuncios.jsx
+++ src/containers/Anuncios.jsx
@@ -5,32 +5,31 @@ import '@styles/Anuncios.scss';
 const Anuncios = () => {
     return (
         <section className='container'>
-            <div className="title">
+            {/* <div className="title">
                 <h3>Tiendas del pueblo</h3>
-            </div>
+            </div> */}
 
             <div className="stores">
-                <div className="tipos">
-                    <img src='../assets/img/shop.png' alt="" /> 1
-                </div>
-                <div className="tipos">
-                    <img src='../assets/img/shop.png' alt="" /> 2
-                </div>
-                <div className="tipos">
-                    <img src='../assets/img/shop.png' alt="" /> 3
-                </div>
-                <div className="tipos">
-                    <img src='../assets/img/shop.png' alt="" /> 4
-                </div>
-                <div className="tipos">
-                    <img src='../assets/img/shop.png' alt="" /> 5
-                </div>
-                <div className="tipos">
-                    <img src='../assets/img/shop.png' alt="" /> 6
-                </div>
-                <div className="tipos">
-                    <img src='../assets/img/shop.png' alt="" /> 6
-                </div>
+                <ul className='store-listado'>
+                    <li className='tipos'>
+                        <img src='../assets/img/shop.png' alt="" /> 1
+                    </li>
+                    <li className='tipos'>
+                        <img src='../assets/img/shop.png' alt="" /> 1
+                    </li>
+                    <li className='tipos'>
+                        <img src='../assets/img/shop.png' alt="" /> 1
+                    </li>
+                    <li className='tipos'>
+                        <img src='../assets/img/shop.png' alt="" /> 1
+                    </li>
+                    <li className='tipos'>
+                        <img src='../assets/img/shop.png' alt="" /> 1
+                    </li>
+                    <li className='tipos'>
+                        <img src='../assets/img/shop.png' alt="" /> 1
+                    </li>
+                </ul>
             </div>
         </section>
     );
