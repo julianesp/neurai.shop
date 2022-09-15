ca74f929161f1d9e79c1b6e89fdaefb506d58850
Julián España <julii1295@gmail.com>
2022-09-14 20:23:41 -0500

    Agregando interactividad a la publicidad

--- src/containers/Publicidad.jsx
+++ src/containers/Publicidad.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import '@styles/Publicidad.scss'
+import '../js/carrusel'
 
 const Publicidad = () => {
     return (
         <section className="carrousel">            
             <div className="grande">
-                <img src='#' alt="" className='img'/>
-                <img src='#' alt="" className='img'/>
-                <img src='#' alt="" className='img'/>
-                <img src='#' alt="" className='img'/>
-                <img src='#' alt="" className='img'/>
+                <img src='../assets/img/developer.jpg' alt="imagen1" className='img'/>
+                <img src='@img/' alt="imagen2" className='img'/>
+                <img src='@img/sena.png' alt="imagen3" className='img'/>
+                <img src='@img/' alt="imagen4" className='img'/>
+                <img src='@img/' alt="imagen5" className='img'/>
             </div>
 
             <ul className="puntos">
-                <li className="punto"></li>
-                <li className="punto"></li>
+                <li className="punto activo"></li>
+                <li className="punto activo"></li>
+                <li className="punto activo"></li>
+                <li className="punto activo"></li>
+                <li className="punto activo"></li>
             </ul>
         </section>
     );
---
+++ src/js/carrusel.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const grande = document.querySelector('.grade')
+const punto = document.querySelectorAll('.punto')
+
+// Recorrer todos los puntos
+punto.forEach( (cadaPunto, i ) => {
+    //save position of that point
+    let posicion = i
+
+    // calculando el espacio que debe desplazarse el grande
+    let operacion = posicion * -50
+
+    // Movemos el grande 
+    grande.style.transform = `translateX(${ operacion }%)`
+
+    // Recorremos todos los puntos 
+    punto.forEach( ( cadaPunto, i ) => {
+        // quitamos la clase activo a todos los puntos 
+        punto[i].classList.remove('activo')
+    })
+
+    // añadir la clase activo en el punto que hemos hecho click 
+    punto[i].classList.add('activo')
+})
