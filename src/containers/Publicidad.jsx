89f6de44f332982af6b46d650db3f00b2a0a3d94
Julián España <julii1295@gmail.com>
2022-10-01 20:41:50 -0500

    Agregando control publicidad

--- src/containers/Publicidad.jsx
+++ src/containers/Publicidad.jsx
@@ -2,24 +2,39 @@ import React from 'react';
 import '@styles/Publicidad.scss'
 import '../js/carrusel'
 
+// constante para traer imagenes
+const img = require.context('@img/botones/', true)
+
 const Publicidad = () => {
     return (
         <section className="carrousel">            
             <div className="grande">
                 <img className='img'/>
-                <img src='../assets/img/developer.jpg' className='img'/>
-                <img src='../assets/img/developer.jpg' className='img'/>
-                <img src='../assets/img/developer.jpg' className='img'/>
-                <img src='../assets/img/developer.jpg' className='img'/>
+                <img className='img'/>
+                <img className='img'/>
+                <img className='img'/>
+                <img className='img'/>
             </div>
 
-            <ul className="puntos">
-                <li className="punto activo"></li>
-                <li className="punto"></li>
-                <li className="punto"></li>
-                <li className="punto"></li>
-                <li className="punto"></li>
+            {/* botones control */}
+            <ul className='control'>
+                    <img src={img(`./regresa.png`)} className='left' alt="" />
+                    <img src={img(`./adelante.png`)} className='right' alt="" />
+                {/* <li className='boton left'>
+                    <img src={img(`./regresa.png`)} alt="" />
+                </li>
+                <li className='boton right'>
+                    <img src={img(`./adelante.png`)} alt="" />
+                </li> */}
             </ul>
+
+            {/* <ul className="puntos">
+                <li className="punto activo"></li>
+                <li className="punto activo"></li>
+                <li className="punto activo"></li>
+                <li className="punto activo"></li>
+                <li className="punto activo"></li>
+            </ul> */}
         </section>
     );
 };
