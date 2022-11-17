00107411f4acc48155c05ee8ce90bada50909803
Julián España <julii1295@gmail.com>
2022-11-17 14:18:00 -0500

    Volviendo a la config de React without Next.js

---
+++ src/databases/dataCelulares.ts
@@ -0,0 +1,15 @@
+const data = {
+    'charger-tc': {
+        name: 'Cargador tipo C',
+        id: 'cel-1',
+        price: 35.000,
+        image: '/src/assets/pictures/celulares/usbv/2.png',
+        attributes: {
+            description: 
+                'Carga más rápido tu celular con sus 20W (vatios) en menos tiempo',
+            type: 'Tipo C',            
+        }
+    },
+}
+
+export default data
